e12b9bb119472bb1d4064bf1f98d85faf92600a6
Vladyslav_Chernenko <vladyslav_chernenko@epam.com>
2019-12-17 18:00:00 +0200

    Add Firebase integration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import Modal from '../Modal/Modal';
+  
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
+import { AuthProvider } from "../../Auth";
+import PrivateRoute from "../../PrivateRoute";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Modal />
-    </div>
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={ Home } />
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/signup" component={ SignUp } />
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
